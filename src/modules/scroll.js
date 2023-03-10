636003e4c9355e9cb28dc8771c195b870473d6fb
Chirkov Slava <slavachirkov92@gmail.com>
2023-03-10 18:45:11 +0300

    add scrollBtn

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,19 +1,29 @@
 const scroll = () => {
 	const menu = document.querySelector('menu'),
-		menuItems = menu.querySelectorAll('ul > li > a');
+		menuItems = menu.querySelectorAll('ul > li > a'),
+		main = document.querySelector('main'),
+		scrollBtn = main.querySelector('a');
 
+	const scrollFrom = (elem) => {
+		const href = elem.getAttribute("href"),
+			block = document.querySelector(href);
+
+		block.scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		});
+	}
+	
 	menuItems.forEach((link) => {
 		link.addEventListener('click', (e) => {
 			e.preventDefault();
-
-			const href = link.getAttribute("href"),
-				block = document.querySelector(href);
-
-			block.scrollIntoView({
-				behavior: "smooth",
-				block: "start"
-			});
+			scrollFrom(link);
 		});
+	});
+
+	scrollBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		scrollFrom(scrollBtn);
 	});
 }
 
